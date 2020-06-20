cce06c9baf71b8e7890b67d928169e1bcd3a980b
Enger Jimenez <engersky12@gmail.com>
2020-06-20 09:09:09 -0400

    Added files to repo

---
+++ Foxtrot/Migrations/20200620001216_CreatedServiceAndAppointmentModels.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Foxtrot.Migrations
+{
+    public partial class CreatedServiceAndAppointmentModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "Users",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "Roles",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.CreateTable(
+                name: "Services",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<Guid>(nullable: false),
+                    UpdatedDate = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<Guid>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false, defaultValue: false),
+                    Name = table.Column<string>(maxLength: 100, nullable: false),
+                    Duration = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Services", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Appointments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<Guid>(nullable: false),
+                    UpdatedDate = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<Guid>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false, defaultValue: false),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false),
+                    CreatorId = table.Column<Guid>(nullable: true),
+                    ServiceId = table.Column<Guid>(nullable: true),
+                    ProviderId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Appointments_Users_CreatorId",
+                        column: x => x.CreatorId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Appointments_Users_ProviderId",
+                        column: x => x.ProviderId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Appointments_Services_ServiceId",
+                        column: x => x.ServiceId,
+                        principalTable: "Services",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_CreatorId",
+                table: "Appointments",
+                column: "CreatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_ProviderId",
+                table: "Appointments",
+                column: "ProviderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_ServiceId",
+                table: "Appointments",
+                column: "ServiceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appointments");
+
+            migrationBuilder.DropTable(
+                name: "Services");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldDefaultValue: false);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "Roles",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldDefaultValue: false);
+        }
+    }
+}
---
+++ Foxtrot/Migrations/20200620034052_CreatedAdminUser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Foxtrot.Migrations
+{
+    public partial class CreatedAdminUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            Guid adminId = new Guid("9F917B76-5B52-4DF2-9A9E-D99875777AC4");
+            Guid adminRoleId = new Guid("DC00B091-C6C6-449F-9F0C-2B55AF3E0D34");
+            string emptyGuid = "00000000-0000-0000-0000-000000000000";
+            DateTime now = DateTime.Now;
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[]
+                {
+                    "Id", "Name", "Description", "CreatedDate", "CreatedBy", 
+                    "UpdatedDate", "UpdatedBy"
+                },
+                values: new object[]
+                {
+                    adminRoleId, "Admin", "I'm the creator of everything",
+                    now, emptyGuid, now, emptyGuid
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[]
+                {
+                    "Id", "FullName", "Email", "Address", "Dni", "RoleId",
+                    "CreatedDate", "CreatedBy", "UpdatedDate", "UpdatedBy"
+                    
+                },
+                values: new object[] {
+                    adminId, "Enger Jimenez", "ejimenezr21@gmail.com", 
+                    "South Carolina, Townville, 455  Mill Street", "62464787F", adminRoleId,
+                    now, emptyGuid, now, emptyGuid,
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
