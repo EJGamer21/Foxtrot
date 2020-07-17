92522869dc65a39c66c56643cb411af7af2a12ab
Enger Jimenez <engersky12@gmail.com>
2020-07-17 16:51:46 -0400

    Added session usage

--- Foxtrot/Extensions/HttpContextExtension.cs
+++ Foxtrot/Extensions/HttpContextExtension.cs
@@ -8,15 +8,20 @@ namespace Foxtrot.Extensions
     {
         public static Guid GetLoggedUserId(this HttpContext httpContext)
         {
+            var userId = httpContext.Session.GetString("UserId");
+            return string.IsNullOrWhiteSpace(userId)
+                ? Guid.Empty
+                : new Guid(userId);
+            
             /*return httpContext.User == null
                 ? Guid.Empty
                 : new Guid(httpContext.User.Claims
                     .Single(claim => claim.Type == "sid").Value);*/
-            
-            Guid userId = new Guid(httpContext.User.Claims
+
+            /*Guid userId = new Guid(httpContext.User.Claims
                 .FirstOrDefault(claim => claim.Type == "sid")?.Value ?? string.Empty);
 
-            return new Guid("9F917B76-5B52-4DF2-9A9E-D99875777AC4");
+            return new Guid("9F917B76-5B52-4DF2-9A9E-D99875777AC4");*/
         }
     }
 }
