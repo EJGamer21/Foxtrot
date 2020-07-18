9418a6051f1c5a6624e150127fc9420459a726a8
Enger Jimenez <engersky12@gmail.com>
2020-07-18 00:53:40 -0400

    added missing files

--- Foxtrot/Extensions/HttpContextExtension.cs
+++ Foxtrot/Extensions/HttpContextExtension.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using Microsoft.AspNetCore.Http;
 
 namespace Foxtrot.Extensions
@@ -12,16 +11,9 @@ namespace Foxtrot.Extensions
             return string.IsNullOrWhiteSpace(userId)
                 ? Guid.Empty
                 : new Guid(userId);
-            
-            /*return httpContext.User == null
-                ? Guid.Empty
-                : new Guid(httpContext.User.Claims
-                    .Single(claim => claim.Type == "sid").Value);*/
-
-            /*Guid userId = new Guid(httpContext.User.Claims
-                .FirstOrDefault(claim => claim.Type == "sid")?.Value ?? string.Empty);
-
-            return new Guid("9F917B76-5B52-4DF2-9A9E-D99875777AC4");*/
         }
+
+        public static bool IsUserLoggedIn(this HttpContext httpContext) =>
+            GetLoggedUserId(httpContext) != Guid.Empty;
     }
 }
